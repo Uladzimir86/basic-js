c04e6045df9e1c7388034ff3516de5b60b6c469e
Uladzimir86 <olodja@rambler.ru>
2021-02-24 23:52:46 +0300

    feat: add solution to ### **Transform array**

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(ar) {
+
+  
+  
+  let arr = ar;
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] == `--discard-prev` && i == 0) {
+      arr.splice(0,1);
+      i = -1;
+    }
+    else if (arr[i] == `--double-prev` && i == 0) {
+      arr.splice(0,1);
+      i = -1;}
+    else if (arr[i] == `--discard-prev` && i != 0) {
+      arr.splice(i-1,2);
+      i = i-2;}
+    else if (arr[i] == `--double-prev` && i != 0) {arr.splice(i,1,arr[i-1]);}
+
+    
+    
+    else if (arr[i] == `--discard-next` && i != arr.length-1) {
+      arr.splice(i,2,' ');
+    }
+    else if (arr[i] == `--double-next` && i != arr.length-1) {arr.splice(i,1,arr[i+1]);}  
+    
+    else if (arr[i] == `--double-next` && i == arr.length-1) {arr.splice(i,1);}  
+    else if (arr[i] == `--discard-next` && i == arr.length-1) {arr.splice(i,1);}
+    
+  }
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] == " ") {
+      arr.splice(i,1);
+      i-=1;
+    }
+  }
+if(!Array.isArray(ar)) return "Error";
+return arr;
+}
