307c0f66e914dc9b2723c0ce967f3d49b0e4ccd5
Uladzimir86 <olodja@rambler.ru>
2021-03-02 21:47:06 +0300

    fix: add recurcive to **Recursive depth calculator

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,17 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
- calculateDepth(arr) {
-    let depth = 1;
-    let n=0
-   if(Array.isArray(arr)) {
+ calculateDepth(arr, x=0) { 
+    //let arr1 = arr; 
+    let depth = x;
+    
+    console.log('arr'+arr);
+    console.log('dep'+depth);
+    let resArr = [];
+    ///enrenlnglenw;gnb;wnih'oqgn3'n'3q4nqnbv
+    if (depth <= 2) return this.calculateDepth(arr,x+1);
+    
+   function calc(arr, n)  {
+        
+    if (arr.length == 0) {
+         resArr.push(n);}
+      else  { arr.forEach((item,index,array) => { 
+        
+        
+      if(Array.isArray(item) ) {console.log(item+'----'+n);
+        calc(item, n+1);
+      }
+      if (index == array.length -1) {
+        console.log(item+'if----+++'+n); 
+        resArr.push(n);
+      }
      
-     arr.forEach((item,index,array) => { 
-       if(Array.isArray(item)) depth+=this.calculateDepth(item);
-    })
-     
-   }
+      
+    })}}
+   
+ calc (arr, 1);
   
+  console.log('resarr'+resArr);
+    resArr.forEach(item => {if(depth < item) depth = item} ) 
+
    return depth;
  } 
 }
