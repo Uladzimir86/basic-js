dac0b8fe02a975895f4969d7127b8ba2391b340d
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2024-12-31 00:51:59 +0300

    solve VigenereCipheringMachine

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -16,19 +16,13 @@ const chainMaker = {
     return this;
   },
   removeLink(position) {
-    try {
-      const pos = position - 1;
-      // console.log('this.chain: ', this.chain);
-      console.log('pos: ', position);
-      if (typeof pos !== 'number' || pos < 0 || position > this.chain.length) throw new Error();
-      this.chain.splice(pos, 1);
-      return this;
-    } catch(e) {
-      console.log('e: ', e.message);
-      // console.log(111);
+    const pos = position - 1;
+    if (typeof pos !== 'number' || pos < 0 || position > this.chain.length) {
       this.chain = [];
-      throw new Error('You can\'t remove incorrect link!')
+      throw new Error('You can\'t remove incorrect link!');
     }
+    this.chain.splice(pos, 1);
+    return this;
   },
   reverseChain() {
     this.chain.reverse();
@@ -41,11 +35,6 @@ const chainMaker = {
   }
 };
 
-// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
-// console.log('chainMaker1: ', chainMaker.addLink(1).addLink(2).addLink(3).removeLink(4));
-// console.log('chainMaker: ', chainMaker.reverseChain().addLink('ABC').reverseChain().reverseChain().reverseChain().addLink(Infinity).addLink(false).addLink(0).addLink('8.963').removeLink(2).removeLink(1).reverseChain().finishChain());
-
-// '( 3rd )~~( function () { } )'
 module.exports = {
   chainMaker
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  codeA = 65;
+  codeZ = 90;
+
+  constructor (isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  errorCheck(value, key) {
+    if (!value || !key) throw new Error('Incorrect arguments!');
+  }
+
+  encrypt(value, key) {
+    this.errorCheck(value, key);
+    const valueUp = value.toUpperCase();
+    const keyUp = key.toUpperCase();
+    const keyLen = key.length;
+    let valueSpaces = 0;
+    const encryptVal = [...valueUp].map((item, ind) => {
+      const itemCode = item.codePointAt(0);
+      if (itemCode < this.codeA || itemCode > this.codeZ) {
+        valueSpaces += 1;
+        return item;
+      }
+      const keyInd = ind - valueSpaces < keyLen ? ind - valueSpaces : (ind - valueSpaces) % keyLen;
+      const currentCode = itemCode + keyUp.codePointAt(keyInd) - this.codeA;
+      const count = this.codeZ - currentCode;
+      const newCode = count < 0 ? this.codeA - count - 1 : currentCode;
+      return String.fromCodePoint(newCode).toUpperCase();
+    }) 
+    return this.isDirect ? encryptVal.join('') : encryptVal.reverse().join('');
+  }
+
+  decrypt(value, key) {
+    this.errorCheck(value, key);
+    const valueUp = value.toUpperCase();
+    const keyUp = key.toUpperCase();
+    const keyLen = key.length;
+    let valueSpaces = 0;
+    const decryptVal = [...valueUp].map((item, ind) => {
+      const itemCode = item.codePointAt(0);
+      if (itemCode < this.codeA || itemCode > this.codeZ) {
+        valueSpaces += 1;
+        return item;
+      }
+      const keyInd = ind - valueSpaces < keyLen ? ind - valueSpaces : (ind - valueSpaces) % keyLen;
+      const currentCode = itemCode - keyUp.codePointAt(keyInd) + this.codeA;
+      const count = this.codeA - currentCode;
+      const newCode = count > 0 ? this.codeZ - count + 1 : currentCode;
+      return String.fromCodePoint(newCode).toUpperCase();
+    }) 
+    return this.isDirect ? decryptVal.join('') : decryptVal.reverse().join('');
+
   }
 }
 
